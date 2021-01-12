0b53d5ac0d0fc6467c8c501b506cc24b4f37f440
Lidia Alonso <l.alonsodiaz@gmail.com>
2021-01-12 20:28:59 +0100

    added js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //bring panels into JS files
+const panels = document.querySelectorAll('.panel');
+
+//loop over the panels
+panels.forEach((panel) => {
+	// console.log(panel);
+	panel.addEventListener('click', () => {
+		// console.log(123);
+
+		//remove active classes
+		removeActiveClasses();
+		//add class to panel every click
+		panel.classList.add('active');
+	});
+});
+
+function removeActiveClasses() {
+	//loop over panels
+
+	panels.forEach((panel) => {
+		panel.classList.remove('active');
+	});
+}
